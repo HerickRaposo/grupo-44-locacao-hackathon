38a77a7fc23e9e84d80c34f5453ae0330281d168
ernesto.sambango <ernesto.sambongo@outlook.com>
2024-03-14 21:01:57 +0100

    ACERTO DO SERVIÇO DE ATUALIZAÇÃO.

--- src/main/java/com/fiap/locacao/dominio/gestao/servicoseopcionais/service/impl/ServicosItensServiceImpl.java
+++ src/main/java/com/fiap/locacao/dominio/gestao/servicoseopcionais/service/impl/ServicosItensServiceImpl.java
@@ -1,6 +1,7 @@
 package com.fiap.locacao.dominio.gestao.servicoseopcionais.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,21 +11,38 @@ import com.fiap.locacao.dominio.gestao.servicoseopcionais.dto.ServicosItensDTOou
 import com.fiap.locacao.dominio.gestao.servicoseopcionais.entity.ServicosItens;
 import com.fiap.locacao.dominio.gestao.servicoseopcionais.repository.IServicosItensRepository;
 import com.fiap.locacao.dominio.gestao.servicoseopcionais.service.ServicosItensService;
+import com.fiap.locacao.exception.ControllerNotFoundException;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ServicosItensServiceImpl implements ServicosItensService{
 	private @Autowired IServicosItensRepository iServicosItensRepository;
 
+	@Transactional
 	@Override
 	public ServicosItensDTOout salvar(ServicosItensDTOin servicosItensDTO) {
 		ServicosItens servicosItens = this.iServicosItensRepository.save(servicosItensDTO.getEntity());
 		return new ServicosItensDTOout().getDTO(servicosItens);
 	}
 
+	@Transactional
 	@Override
 	public ServicosItensDTOout atualizar(ServicosItensDTOin servicosItensDTO,Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<ServicosItens> OservicosItens = this.iServicosItensRepository.findById(id);
+		
+		try {
+			ServicosItens servicosItens = OservicosItens.get();
+			servicosItens.setDescricao(servicosItensDTO.getDescricao());
+			servicosItens.setTipoServicosItens(servicosItensDTO.getTipoServicosItens());
+			servicosItens.setValor(servicosItensDTO.getValor());
+			
+			this.iServicosItensRepository.save(servicosItens);
+			
+			return new ServicosItensDTOout().getDTO(servicosItens);
+		}catch(Exception e) {
+			throw new ControllerNotFoundException("Endereço não encontrado, id: " + id);
+		}	
 	}
 
 	@Override
